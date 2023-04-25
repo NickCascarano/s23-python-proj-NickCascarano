ad952d321e5691f00e4c2e6dcb292af657552555
ArinzeIkenga <ikengad@stthom.edu>
2023-04-24 19:16:04 -0500

    Stats

--- celtsoccer/stats/stats.py
+++ celtsoccer/stats/stats.py
@@ -29,6 +29,7 @@ def most_common_club(primary_data, club_country_data):
             max_club = club
 
     return (max_club, max_minutes)
+
 ################################################################# Finished Above
 
 
@@ -36,46 +37,36 @@ def most_common_club(primary_data, club_country_data):
 # of the player and the number of goals scorer. If there are more than
 # one player with the same number of goals, returns the one at the top alphabetically.
 ################################################################## Working Below
-def top_scorer(data): #this function works, done
-    top_scorers_dict = {}
-    for country, players in data.items():
-        top_scorer_name = ''
-        top_scorer_goals = 0
-        for player_name, player_stats in players.items():
-            player_goals = player_stats[8]
-            if player_goals > top_scorer_goals:
-                top_scorer_name = player_name
-                top_scorer_goals = player_goals
-            elif player_goals == top_scorer_goals and player_name < top_scorer_name:
-                top_scorer_name = player_name
-                top_scorer_goals = player_goals
-        top_scorers_dict[country] = (top_scorer_name, top_scorer_goals)
-    return top_scorers_dict
+def top_scorer(data, country=''):
+  local_max = 0
+  for country_name, player in data.items():
+    if country_name == country:
+      for player_name, player_data in player.items():
+        goals_scored = player_data[8]
+        if goals_scored > local_max:
+          result = (player_name, goals_scored)
+          local_max = goals_scored
+        elif goals_scored < local_max:
+          continue
+        elif goals_scored == local_max:
+          if player_name < result[0]:
+            result = (player_name, goals_scored)
+  return result
 ################################################################## Working Above
 # Computes the average goals scored for players of a given club.
 # The average is the sum of goals for players for that club,
 # divided by the number of players of that club who scored goals
 # (don't count players who didn't score).
-def avg_goals_scored(data):
-
-  club_dict = {}
+def avg_goals_scored(data, club=''):
+  running_total = 0
+  count = 0
   for country, player in data.items():
     for player_name, player_data in player.items():
-      player_club_name = player_data[2]
-      player_goals_scored = player_data[8]
-      if player_club_name not in club_dict:
-        club_dict[player_club_name] = [player_goals_scored, 1]
-      else:
-        club_dict[player_club_name][0] += player_goals_scored
-        club_dict[player_club_name][1] += 1
-
-  result = {}
-  for club_name, goals_count in club_dict.items():
-    goals_scored = goals_count[0]
-    player_count = goals_count[1]
-    if player_count > 0:
-      result[club_name] = goals_scored / player_count
-  return result
+      if club == player_data[2]:
+        if player_data[8] > 0:
+          running_total += player_data[8]
+          count += 1
+  return (club, round((running_total / count),2))
 
 
 # Returns a list with tuples, in which each entry is a pair (player, minutes played)
